3a280d09cbd5910ee32f7b22fc926ab791a01a51
sanjugowda216 <143921320+sanjugowda216@users.noreply.github.com>
2024-10-24 14:33:22 -0700

    Update a121_catch_a_turtle_SG.py

---
+++ a121_catch_a_turtle_SG.py
@@ -0,0 +1,16 @@
+# a121_catch_a_turtle.py
+#-----import statements-----
+import turtle
+
+#-----game configuration----
+color = "pink"
+shape = 'heart'
+size = 10
+
+#-----initialize turtle-----
+
+
+#-----game functions--------
+
+
+#-----events----------------
